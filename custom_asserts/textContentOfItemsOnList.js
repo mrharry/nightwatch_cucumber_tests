db6dd8561e182933d0140c4e87bf3a8e22b6df45
mrharry <stuart_a_robertson@hotmail.com>
2017-09-11 05:14:09 +0100

    initial commit

--- custom_asserts/textContentOfItemsOnList.js
+++ custom_asserts/textContentOfItemsOnList.js
@@ -1,64 +1,55 @@
 /**
- * Checks if the element has the expected number of elements.
- *
- * ```
- *    this.demoTest = function (client) {
- *      browser.assert.countOfItemsOnList('css selector', '.class', 'button', 'some value');
- *    };
- * ```
- *
- * @method attributeEquals
- * @param {string} selector The selector (CSS / Xpath) used to locate the element.
- * @param {string} element The element selector (id, class).
- * @param {string} attribute The attribute name (button, li)
- * @param {string} expected The expected value.
- * @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
- * @api assertions
- */
+* Checks if the element has the expected text for the child elements.
+*
+* ```
+*    this.demoTest = function (client) {
+*      browser.assert.textContentOfItemsOnList('css selector', '.class', 'button', 'some value');
+*    };
+* ```
+*
+* @method attributeEquals
+* @param {string} selector The selector (CSS / Xpath) used to locate the element.
+* @param {string} element The element selector (id, class).
+* @param {string} attribute The attribute name (button, li)
+* @param {string} expected The expected text values.
+* @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
+* @api assertions
+*/
 
 var util = require('util');
 exports.assertion = function(selector, element, attribute, expected, msg) {
 
     const element_with_attribute  = element + ' ' + attribute;
-    let found = [];
-
-    const DEFAULT_MSG = 'Testing the element <%s> with the attribute "%s" has "%s" items.';
+    
+    const DEFAULT_MSG = 'Testing the element <%s> with the attribute "%s" contains these items "%s".';
 
     this.message = msg || util.format(DEFAULT_MSG, element, attribute, expected);
 
     this.expected = function() {
-
         return expected;
     };
 
     this.pass = function(value) {
-        console.log("inside pass  " + JSON.stringify(value))
-
+        return value === expected;
     };
 
     this.value = function(result) {
-        console.log("inside result  " + JSON.stringify(result));
-        return result
+        console.log("in this value with a result" + JSON.stringify(result))
+        return result.value;
     };
 
     this.command = function(callback) {
-
-        self = this.api;
-        this.api.elements(selector, element_with_attribute, function(res) {
-            let items_processed = 0;
-            res.value.forEach(function(el) {
-                self.elementIdText(el.ELEMENT, function (res_text) {
-                    found.push(res_text.value);
-                    items_processed++;
-                    if(items_processed === res.value.length) {
-                        callback(found);
-                    }
+        const self = this.api;
+        
+        this.api.elements('css selector', element_with_attribute, function(el) {
+            el.value.forEach(function(element) {
+                self.elementIdText(element.ELEMENT, callback, function (result) {
+                    console.log("result is " + result.value)
+                    return "blah"
                 });
-
             });
+        })
 
-        });
     };
+
 };
-
-
