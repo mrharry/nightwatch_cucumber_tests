0db367f7910eaf4f9421c9c6726231c47cc52fa1
mrharry <stuart_a_robertson@hotmail.com>
2017-09-24 07:33:01 +0100

    add Then step to check outcome on PO example

--- custom_asserts/textContentOfItemsOnList.js
+++ custom_asserts/textContentOfItemsOnList.js
@@ -1,56 +1,64 @@
 /**
-* Checks if the element has the expected text for the child elements.
-*
-* ```
-*    this.demoTest = function (client) {
-*      browser.assert.textContentOfItemsOnList('css selector', '.class', 'button', 'some value');
-*    };
-* ```
-*
-* @method attributeEquals
-* @param {string} selector The selector (CSS / Xpath) used to locate the element.
-* @param {string} element The element selector (id, class).
-* @param {string} attribute The attribute name (button, li)
-* @param {string} expected The expected text values.
-* @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
-* @api assertions
-*/
+ * Checks if the element has the expected number of elements.
+ *
+ * ```
+ *    this.demoTest = function (client) {
+ *      browser.assert.countOfItemsOnList('css selector', '.class', 'button', 'some value');
+ *    };
+ * ```
+ *
+ * @method attributeEquals
+ * @param {string} selector The selector (CSS / Xpath) used to locate the element.
+ * @param {string} element The element selector (id, class).
+ * @param {string} attribute The attribute name (button, li)
+ * @param {string} expected The expected value.
+ * @param {string} [message] Optional log message to display in the output. If missing, one is displayed by default.
+ * @api assertions
+ */
 
 var util = require('util');
 exports.assertion = function(selector, element, attribute, expected, msg) {
 
     const element_with_attribute  = element + ' ' + attribute;
+    let found = [];
 
-    const DEFAULT_MSG = 'Testing the element <%s> with the attribute "%s" contains these items "%s".';
+    const DEFAULT_MSG = 'Testing the element <%s> with the attribute "%s" has "%s" items.';
 
     this.message = msg || util.format(DEFAULT_MSG, element, attribute, expected);
 
     this.expected = function() {
+        
         return expected;
     };
 
     this.pass = function(value) {
-        return value === expected;
+
+        self = this.api;
+        text_found = foundText(value)
+        console.log("back from new function res is " + text_found)
+        
+        function foundText(value) {
+            value.value.forEach(function (element) {
+                return self.elementIdText(element.ELEMENT, function (res) {
+                    found.push(res.value)
+                    console.log("inside text loop  " + JSON.stringify(found))
+                    return found
+                });
+                return
+            });
+            return "blah"
+        }
+        
+        
     };
 
     this.value = function(result) {
-        console.log("in this value with a result" + JSON.stringify(result))
-        return result.value;
+ 
+        return result
     };
 
     this.command = function(callback) {
-        const self = this.api;
-
-        this.api.elements('css selector', element_with_attribute, function(el) {
-            el.value.forEach(function(element) {
-                self.elementIdText(element.ELEMENT, callback, function (result) {
-                    console.log("result is " + result.value)
-
-                });
-                return this
-            });
-        })
-
+        return this.api.elements(selector, element_with_attribute, callback);
     };
 
 };
--- features/menu.feature
+++ features/menu.feature
@@ -4,16 +4,16 @@ Feature:
     When I click on the More button
     Then I will see 9 sports
 
- # Scenario: The content of a list
-  #  Given I have opened the Sports page
-  #  When I click on the More button
-  #  Then I will see these sports
-  #  | Premier League |
-  #  | Scottish Prem  |
-  #  | Championship   |
+  Scenario: The content of a list
+    Given I have opened the Sports page
+    When I click on the More button
+    Then I will see these sports
+    | Premier League |
+    | Scottish Prem  |
+    | Championship   |
 
-    Scenario: selecting an item on a list
-      Given I have opened the Sports page
-      And I click on the More button
-      When I select the "Championship"
-      Then the "Championship" is displayed
+  Scenario: selecting an item on a list
+    Given I have opened the Sports page
+    And I click on the More button
+    When I select the "Championship"
+    Then the "Championship" is displayed
