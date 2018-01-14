1c85e8572d6d64bd5954338763c88f55380642d6
mrharry <stuart_a_robertson@hotmail.com>
2018-01-14 21:31:28 +0000

    fix typo on filename

--- getRedmineTickets.rb
+++ getRedmineTickets.rb
@@ -76,6 +76,45 @@ def write_csv(csv, csv_risk, row)
 
 end
 
+def manual(test_runs_manual, test_case, row, csv, csv_risk)
+  test_runs_manual.each do |test_run|
+    if test_run["parent"]["id"] == test_case["id"]
+      add_test_run_manual(row, test_run)
+      write_csv(csv, csv_risk, row)
+      create_redmine_ticket(test_run)
+    end
+  end
+end
+
+def create_redmine_ticket(test_run)
+  puts test_run["subject"]
+
+  # Creating an issue
+  issue = Issue.new(
+    :subject => test_run["subject"],
+    :project_id => 2,
+    :tracker_id => 7
+  # custom field with id=2 exist in database
+  #  :custom_fields => [{id: 2, value: "IT"}]
+  )
+  if issue.save
+    puts issue.id
+  else
+    puts issue.errors.full_messages
+  end
+
+
+end
+
+def automation(test_runs_auto_hash, requirement, row, csv, csv_risk)
+  test_runs_auto_hash.each do |test_run_auto|
+    if test_run_auto["feature"] == requirement["id"]
+      add_test_run_auto(row, test_run_auto)
+    end
+  end
+  write_csv(csv, csv_risk, row)
+end
+
 requirements = JSON.parse(retrieve_an_issue(2,10).to_json).sort_by! { |hash| hash['id'].to_i }
 test_features = JSON.parse(retrieve_an_issue(2,9).to_json).sort_by! { |hash| hash['id'].to_i }
 test_cases = JSON.parse(retrieve_an_issue(2,11).to_json).sort_by! { |hash| hash['id'].to_i }
@@ -103,19 +142,9 @@ CSV.open("risks_build_#{ARGV[3]}-#{getTime}.csv", "wb") do |csv_risk|
             test_cases.each do |test_case|
               if test_case["parent"]["id"] == test_feature["id"]
                 add_test_case(row, test_case)
-                test_runs_manual.each do |test_run|
-                  if test_run["parent"]["id"] == test_case["id"]
-                    add_test_run_manual(row, test_run)
-                    write_csv(csv, csv_risk, row)
-                  end
-                end
+                manual(test_runs_manual, test_case, row, csv, csv_risk)
                 if row.length == 6 # no test run manual, so check for automation
-                  test_runs_auto_hash.each do |test_run_auto|
-                    if test_run_auto["feature"] == requirement["id"]
-                      add_test_run_auto(row, test_run_auto)
-                    end
-                  end
-                  write_csv(csv, csv_risk, row)
+                  automation(test_runs_auto_hash, requirement, row, csv, csv_risk,)
                 end
               end
             end
